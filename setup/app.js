b64ac994adc8d211595b9a1313590238f6d02b87
47xCoding <74483645+47xCoding@users.noreply.github.com>
2021-06-10 17:47:59 +0600

    Used traversing the dom method.
    Changed previous selecting element method to traversing the dom method. And now we can close other question when opening a question.

--- setup/app.js
+++ setup/app.js
@@ -1,24 +1,36 @@
 // traversing the dom
 
+// select item
+const questions = document.querySelectorAll(".question");
 
+// add event
+questions.forEach( function (question) {
+ const btn = question.querySelector(".question-btn")
 
+ btn.addEventListener('click', function () {
+  
+  questions.forEach( function (item) {
+   if (item !== question) {
+    item.classList.remove("show-text");
+   }
+  });
 
-
-
-
+  question.classList.toggle("show-text");
+ });
+});
 
 
 
 //using selectors inside the element
 
-// select item
-const btns = document.querySelectorAll(".question-btn");
+// // select item
+// const btns = document.querySelectorAll(".question-btn");
 
-// add event
-btns.forEach( function (btn) {
- btn.addEventListener('click', function (e) {
-  const question = e.currentTarget.parentElement.parentElement;
-  question.classList.toggle("show-text");
-  console.log(question);
- })
-})
+// // add event
+// btns.forEach( function (btn) {
+//  btn.addEventListener('click', function (e) {
+//   const question = e.currentTarget.parentElement.parentElement;
+//   question.classList.toggle("show-text");
+//   console.log(question);
+//  })
+// })
