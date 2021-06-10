2ad7378bc9060150ca18c9b28df4a61c8d3dcc8e
47xCoding <74483645+47xCoding@users.noreply.github.com>
2021-06-10 11:14:53 +0600

    Added necessary  scripts.
    Added all the necessary scripts to show and hide question texts.

--- setup/app.js
+++ setup/app.js
@@ -1,2 +1,24 @@
-//using selectors inside the element
 // traversing the dom
+
+
+
+
+
+
+
+
+
+
+//using selectors inside the element
+
+// select item
+const btns = document.querySelectorAll(".question-btn");
+
+// add event
+btns.forEach( function (btn) {
+ btn.addEventListener('click', function (e) {
+  const question = e.currentTarget.parentElement.parentElement;
+  question.classList.toggle("show-text");
+  console.log(question);
+ })
+})
